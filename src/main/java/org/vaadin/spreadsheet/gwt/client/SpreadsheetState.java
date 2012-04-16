36ffa32c47f95f97742e1bcdec2c96da82db577a
Joonas Lehtinen <jole@jole.fi>
2012-04-16 23:27:55 +0300

    Commenting code, adding proper column naming 'A', 'B', ..

--- src/main/java/org/vaadin/spreadsheet/gwt/client/SpreadsheetState.java
+++ src/main/java/org/vaadin/spreadsheet/gwt/client/SpreadsheetState.java
@@ -7,70 +7,100 @@ import com.vaadin.terminal.gwt.client.ComponentState;
 public class SpreadsheetState extends ComponentState implements
 		SpreadsheetModel {
 
-	// Key: <row>,<col>. For example "2,3"
-	HashMap<String, String> cellContentsHtml = new HashMap<String, String>();
-	int rows;
-	int cols;
-	boolean graphEnabled = false;
-
+	/** Map from toKey() to cell contents as HTML */ 
+	private HashMap<String, String> cellContentsHtml = new HashMap<String, String>();
 	
+	/** Number of rows in the spreadsheet */
+	private int rows;
+	
+	/** Number of columns in the spreadsheet */
+	private int cols;
+	
+	/** Is drawing of graph enable */
+	private boolean graphEnabled = false;
+
+	/** Map row and column to key used in cellContentsHtml map */
+	private static String toKey(int row,int col) {
+		return row + "," + col;
+	}
+	
+	/** Is the floating graph drawn */
 	public boolean isGraphEnabled() {
 		return graphEnabled;
 	}
 
+	/** Set if the floating graph is drawn */
 	public void setGraphEnabled(boolean graphEnabled) {
 		this.graphEnabled = graphEnabled;
 	}
 
+	/** Get contents of all cells as toKey() -> HTML as String map */
 	public HashMap<String, String> getCellContentsHtml() {
 		return cellContentsHtml;
 	}
 
+	/** Set contents of all cells as toKey() -> HTML as String map */
 	public void setCellContentsHtml(HashMap<String, String> cellContentsHtml) {
 		this.cellContentsHtml = cellContentsHtml;
 	}
 
+	/** Set the number of rows in the spreadsheet */
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
 
+	/** Set the number of columns in the spreadsheet */
 	public void setCols(int cols) {
 		this.cols = cols;
 	}
 
+	/** Get the number of rows in the spreadsheet */
 	public int getRows() {
 		return rows;
 	}
 
+	/** Get the number of rows in the spreadsheet */
 	public int getCols() {
 		return cols;
 	}
 
-	public int getRowHeight(int row) {
+	/** Currently all rows are of the same height. */
+public int getRowHeight(int row) {
 		return 20;
 	}
 
+/** Currently all columns are of the same width. */
 	public int getColWidth(int col) {
 		return 200;
 	}
 
+	/** Get column header for column indexed 1.. */
 	public String getColHeader(int col) {
-		return "" + col;
+		String h = "";
+		while (col>0) {
+			h=(char)('A' + (col-1)%26) + h;
+			col=(col-1)/26;
+		}
+		return h;
 	}
 
+	/** Get row header for rows indexed 1.. */
 	public String getRowHeader(int row) {
 		return "" + row;
 	}
 
+	/** Get the contents of the cell as HTML. Rows and cols are indexed from 1. */
 	public String getCellHtml(int row, int col) {
-		String html = cellContentsHtml.get(row + "," + col);
+		String html = cellContentsHtml.get(toKey(row,col));
 		return html == null ? "" : html;
 	}
 
+	/** Set the contents of the cell as HTML. Rows and cols are indexed from 1. */
 	public void setCellHtml(int row, int col, String html) {
-		cellContentsHtml.put(row + "," + col, html);
+		cellContentsHtml.put(toKey(row,col), html);
 	}
 
+	/** Cell styles are not supported at the moment - just empty styles are returned. */
 	public String getCellStyle(int row, int col) {
 		return "";
 	}
